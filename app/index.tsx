d3ca5d0ee8dbafabb6a65623241faee134d335fd
montague27 <sunnywongs523@gmail.com>
2025-06-22 21:02:20 -0400

    test: react native deep linking source

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,10 @@
 import {Redirect} from 'expo-router';
 import React from "react";
+import {Linking} from "react-native";
 
+Linking.addEventListener('url', (event) => {
+  console.log('Deep link triggered:', event.url);
+});
 export default function Index() {
   return <Redirect href="/(auth)/main"/>;
 }
