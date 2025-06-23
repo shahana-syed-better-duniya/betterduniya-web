1d25c8c4f9e125b6eae855dd523fedee1c1eb51e
montague27 <sunnywongs523@gmail.com>
2025-06-23 14:29:17 -0400

    fix: redirect scheme

--- hooks/auth/use-login-google.ts
+++ hooks/auth/use-login-google.ts
@@ -1,18 +1,12 @@
 import {authorize} from "react-native-app-auth";
 import {Alert} from "react-native";
+import {oauthConfig} from "@/constants/oauth";
 
-const GOOGLE_OAUTH_APP_GUID = '365559828766-e3ebe5cujt9r4s7vb6o2v1rfbuojgs4v';
-const config = {
-  issuer: 'https://accounts.google.com',
-  clientId: `${GOOGLE_OAUTH_APP_GUID}.apps.googleusercontent.com`,
-  redirectUrl: `com.googleusercontent.apps.${GOOGLE_OAUTH_APP_GUID}:/oauth2redirect/google`,
-  scopes: ['openid', 'profile'],
-};
 
 const useLoginGoogle = () => {
   const onLogin = async () => {
     try {
-      const result = await authorize(config);
+      const result = await authorize(oauthConfig);
       console.log(result.accessToken);
       Alert.alert(result.accessToken);
     } catch (error) {
