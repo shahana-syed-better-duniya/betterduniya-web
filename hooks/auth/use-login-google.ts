0bf0ed620826e0c06650132962958fde121a320d
montague27 <sunnywongs523@gmail.com>
2025-06-24 09:37:19 -0400

    update: oauth request

--- hooks/auth/use-login-google.ts
+++ hooks/auth/use-login-google.ts
@@ -1,14 +1,26 @@
 import {authorize} from "react-native-app-auth";
-import {Alert} from "react-native";
 import {oauthConfig} from "@/constants/oauth";
+import useRequest from "@/hooks/api/use-request";
+import {userApi} from "@/api/user/user";
+import {UserLoginSuccessInfo} from "@/interfaces/users/userLoginSuccessInfo";
+import useLoginSave from "@/hooks/auth/use-login-save";
 
 
 const useLoginGoogle = () => {
+  const {onRequest} = useRequest<UserLoginSuccessInfo>();
+
+  const onSave = useLoginSave();
+
   const onLogin = async () => {
     try {
       const result = await authorize(oauthConfig);
-      console.log(result.accessToken);
-      Alert.alert(result.accessToken);
+      const body = {credential: result.idToken};
+      const response = await onRequest(userApi.loginAccountByGoogle, [], body, false)
+      if (response.result) {
+        await onSave(response.result);
+      } else {
+        console.error('UserLoginSuccessInfo is null');
+      }
     } catch (error) {
       console.log(error);
     }
